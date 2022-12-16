aa3c74e06cc8eabc5f8a005e470aac0b970e23b8
Jesse Felix <103081336+JesseDevs@users.noreply.github.com>
2022-12-15 18:49:39 -0800

    Add my app files to test upload

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+import fileSystem from 'node:fs';
+import express from 'express';
+import pokesData from './data/pokemon-data.json' assert {type: 'json'}
+
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.static('css'));
+
+
+app.listen(process.env.PORT || 1994, function () {
+    console.log("Server starting... localhost:1994")
+});
+
+app.get('/', (req, res) => {
+    res.render('pages/home');
+});
+
+app.get('/pokemons', (req, res) => {
+    res.render('pages/pokemons', { pokemons: pokesData });
+});
+
+app.get('/pokemon/:id', (req, res) => {
+
+    const pokemon = pokesData.find(function (pokemon) {
+        return pokemon.id == req.params.id
+    })
+    res.render('pages/pokemon', { pokemon });
+});
+
+
+app.use(function (req, res) {
+    res.status(404).render('pages/404', { query: req.url })
 })
-app.listen(process.env.PORT || 3000)
+
